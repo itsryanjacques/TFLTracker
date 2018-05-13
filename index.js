2f6c39dbc35a7baad42dab79919845a45ca3cb74
Ryan Jacques <ryanthomasjacques@gmail.com>
2018-05-13 18:33:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var app = angular.module("ServiceStatus", []);
+
+app.controller("mainController", function($scope, $http) {
+  var successFunction = function(response) {
+    $scope.data = response;
+  };
+
+  $http
+    .get(
+      "https://api.tfl.gov.uk/Line/bakerloo,central,circle,district,dlr,hammersmith-city,jubilee,london-overground,metropolitan,northern,piccadilly,victoria,waterloo-city,tfl-rail/Status/"
+    )
+    .then(
+      function(response, status, headers, config) {
+        successFunction(response.data);
+      },
+      function(response) {
+        errorFunction(response);
+      }
+    );
+});
+
+$(function() {
+  $(".filters a").click(function() {
+    var activeItem = $(this).parents("li");
+    $(".filters li").removeClass("is-active");
+    activeItem.addClass("is-active");
+      
+    var modeFilter = $(this).data("filter");
+    $(".status .columns").hide();
+    if (modeFilter == "all") {
+      $(".status .columns").show();
+    } else {
+      $("." + modeFilter).show();
+    }
+  });
+});
